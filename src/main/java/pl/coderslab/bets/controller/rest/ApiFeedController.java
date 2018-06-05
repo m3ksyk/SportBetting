01b5345177a201b071a0a2e9c61f46d550957d2f
KSzymanski <m3ksyk@gmail.com>
2018-06-05 12:36:26 +0200

    working on api

--- src/main/java/pl/coderslab/bets/controller/rest/ApiFeedController.java
+++ src/main/java/pl/coderslab/bets/controller/rest/ApiFeedController.java
@@ -2,6 +2,7 @@ package pl.coderslab.bets.controller.rest;
 
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,37 +20,43 @@ public class ApiFeedController {
     //TODO add proper logic for sending json (done lrdy?)
     //TODO add user api-key verification (or don't, but if you don't remove the fuckin comment)
 
-    @PostMapping(path= "/get-leagues")
+    @GetMapping(path= "/get-apikey")
+    public String getApikey() {
+
+        return "apikey";
+    }
+
+    @GetMapping(path= "/get-leagues")
     public ArrayList<JSONObject> getLeaguesAction() {
         ArrayList<JSONObject> leagues = jsonProvider.getLeagues();
         return leagues;
     }
 
-    @PostMapping(path= "/get-sports")
+    @GetMapping(path= "/get-sports")
     public ArrayList<JSONObject> getSportsAction() {
         ArrayList<JSONObject> sports = jsonProvider.getSports();
         return sports;
     }
 
-    @PostMapping(path= "/get-teams")
+    @GetMapping(path= "/get-teams")
     public ArrayList<JSONObject> getTeamsAction() {
         ArrayList<JSONObject> teams = jsonProvider.getTeams();
         return teams;
     }
 
-    @PostMapping(path= "/get-live-games")
+    @GetMapping(path= "/get-live-games")
     public ArrayList<JSONObject> getLiveGamesAction() {
         ArrayList<JSONObject> liveGames = jsonProvider.getLiveGames();
         return liveGames;
     }
 
-    @PostMapping(path= "/get-scheduled-games")
+    @GetMapping(path= "/get-scheduled-games")
     public ArrayList<JSONObject> getScheduledGamesAction() {
         ArrayList<JSONObject> scheduledGames = jsonProvider.getScheduledGames();
         return scheduledGames;
     }
 
-    @PostMapping(path= "/get-games-results")
+    @GetMapping(path= "/get-games-results")
     public ArrayList<JSONObject> getGamesResultsAction() {
         ArrayList<JSONObject> results = jsonProvider.getGamesResults();
         return results;

19a8804716680fb1360226e9d3d257bb79276da9
KSzymanski <m3ksyk@gmail.com>
2018-06-06 18:32:29 +0200

    tests FUBAR

--- src/test/java/pl/coderslab/bets/controller/BetControllerTest.java
+++ src/test/java/pl/coderslab/bets/controller/BetControllerTest.java
@@ -1,21 +1,73 @@
 package pl.coderslab.bets.controller;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import pl.coderslab.bets.service.BetService;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 import static org.junit.Assert.*;
 
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = {BetController.class})
 public class BetControllerTest {
 
-    @Before
-    public void setUp() throws Exception {
+        @MockBean
+        private BetService betService;
+
+        private BetController betController;
+
+        @Before
+        public void setUp(){
+            betController = new BetController();
+        }
+
+    @Test
+    public void placeBet() throws Exception {
+//        String viewName = betController.placeBet();
+//        assertThat("page/index", is(viewName));
+
+//        final String PAGE_NAME = "/game/{id}/bet";
+//        MockMvc mockMvc = standaloneSetup(betController).build();
+//        mockMvc.perform(get("/"))
+//                .andExpect(view().name(PAGE_NAME))
+//                .andExpect(status().isOk());
     }
 
     @Test
-    public void placeBet() {
-    }
+    public void placeBet1() throws Exception {
+//        final String PAGE_NAME = "/game/bet";
+//        MockMvc mockMvc = standaloneSetup(betController).build();
+//        mockMvc.perform(get("/"))
+//                .andExpect(view().name(PAGE_NAME))
+//                .andExpect(status().isOk());
 
-    @Test
-    public void placeBet1() {
+        //old example code
+//        mockMvc.perform(get("", 1L))
+//                .andExpect(status().isOk())
+//                .andExpect(view().name("student/showStudent"))
+//                .andExpect(model().attributeExists("student"))
+//                .andExpect(model().attribute("student",
+//                        hasProperty("lastName", is("Kowalski")));
+//        mockMvc.perform(get("/students/list"))
+//                .andExpect(model().attributeExists("list"))
+//                .andExpect(model().attribute("list", hasSize(3)))
+//                .andExpect(model().attribute("list",
+//                        hasItem(anyOf(hasProperty("firstName"), is("Jan")))))
+//                .andExpect(status().isOk())
+//                .andExpect(content().contentType("text/html;charset=UTF-8"))
+//                .andExpect(content().string(containsString("Janek")))
+//                .andExpect(view().name(STUDENT_LIST_ACTION_VIEW))
+//                .andDo(print());
     }
 }
--- src/test/java/pl/coderslab/bets/controller/IndexControllerTest.java
+++ src/test/java/pl/coderslab/bets/controller/IndexControllerTest.java
@@ -2,20 +2,75 @@ package pl.coderslab.bets.controller;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.ui.ExtendedModelMap;
+import org.springframework.ui.Model;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
+import pl.coderslab.bets.entity.User;
+import pl.coderslab.bets.service.GameService;
+import pl.coderslab.bets.service.UserService;
+
+import java.util.Optional;
+
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.anyOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 import static org.junit.Assert.*;
 
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = {IndexController.class})
 public class IndexControllerTest {
 
+    @MockBean
+    UserService userService;
+
+    @MockBean
+    GameService gameService;
+
+    private IndexController indexController;
+
     @Before
-    public void setUp() throws Exception {
+    public void setUp(){
+        indexController = new IndexController();
     }
 
     @Test
     public void home() {
+        String viewName = indexController.home();
+        assertThat("login", is(viewName));
     }
 
     @Test
-    public void index() {
+    public void index() throws Exception {
+//test doesnt pass because webrequest is not passed in the test
+        MockMvc mockMvc = standaloneSetup(indexController).build();
+        MockHttpServletRequest sr = new MockHttpServletRequest();
+
+        WebRequest request = new ServletWebRequest(sr);
+        when(request.getUserPrincipal().getName()).thenReturn("user");
+
+        mockMvc.perform(get("/index"))
+                .andExpect(view().name("index"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("user"))
+                .andExpect(model().attributeExists("id"))
+                .andExpect(model().attributeExists("liveGames"));
+
     }
 }
